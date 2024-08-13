25d6b01094f45674bf1f3f636844bda05664a44f
Matheus Coelho <104862197+Natare47@users.noreply.github.com>
2024-08-13 12:15:55 -0300

    practically

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const display = document.getElementById("display");
 
+function calculate(){
+       if(Validate(display.value)){
+        display.value = eval(display.value)
+       } else {
+        display.value = 'Error'
+       }
+}
+
+function Validate(expression){
+    try{
+        const result = eval(expression)
+        if(result){
+            if(result == 'NaN' || result == 'Infinity'){
+                return false
+            } 
+            return true
+        } else {
+            return false
+        }
+    } catch(error){
+        return false
+    }
+}
+
 function addNumber(input){
+    if (display.value == 'Error'){
+        display.value = ''
+    } 
     display.value += input;
 }
 
 function clearDisplay(){
     display.value = ''
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value)
-    } catch(error){
-        display.value = 'Error'
-    } if (display.value == 'Infinity'){
-        display.value = 'Error'
-    } else if(display.value == 'NaN'){
-        display.value = 'Error'
-    }
 }
